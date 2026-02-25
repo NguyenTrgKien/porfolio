cc04f6ddbbdb259132bd8343023b7b666b634344
Nguyen Trung Kien <nguyentrungkien040921@gmail.com>
2026-02-25 18:51:05 +0700

    edit url call server

--- frontend/src/configs/axiosConfig.ts
+++ frontend/src/configs/axiosConfig.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://porfolio-oo83.onrender.com",
+  baseURL: "https://porfolio-backend-b8ja.onrender.com",
+  // baseURL: "http://localhost:8080",
   headers: {
     "Content-Type": "application/json",
   },
